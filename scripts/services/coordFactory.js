4e7372f9750582e7d00c430ae40e6346342e7f23
Matthew Neil <mneilweb@gmail.com>
2016-01-01 14:28:46 -0600

    add coordinator factory

---
+++ scripts/services/coordFactory.js
@@ -0,0 +1,34 @@
+angular.module('upl-site').
+    factory('CoordFactory', ['$http', function($http) {
+        var coords = [];
+
+        var service = {};
+
+        service.list = function() {
+            return coords;
+        };
+
+        service.getCoord = function(id) {
+            for (var i = 0, l = coords.length; i < l; i++) {
+                var coord = coords[i];
+                if (coord.id === id) {
+                    return coord;
+                }
+            }
+            return null;
+        };
+
+        service.populate = function() {
+            $http.get({
+                url: 'content/coords/coords.json'
+            }).then(function(response) {
+                // Success
+                coords = response.data;
+            }, function(response) {
+                // Error
+                coords = [];
+            });
+        };
+
+        return service;
+    }]);
