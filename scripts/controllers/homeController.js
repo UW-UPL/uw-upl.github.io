0b4eea7648f5519bfd07a001360672aded93843f
Matthew Neil <mneilweb@gmail.com>
2016-01-26 17:55:36 -0600

    start home page content

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -1,4 +1,19 @@
-angular.module('upl-site').
-    controller('HomeController', ['$scope', function($scope) {
+"use strict";
 
+angular.module('upl-site').
+    controller('HomeController', ['$scope', 'EventsFactory', 'ProjectsFactory', function($scope, Events, Projects) {
+        $scope.events = [];
+        $scope.projects = [];
+
+        Events.list().then(function(data) {
+            $scope.events = data;
+        }, function(data) {
+            alert(data);
+        });
+
+        Projects.list().then(function(data) {
+            $scope.projects = data;
+        }, function(data) {
+            alert(data);
+        });
     }]);
