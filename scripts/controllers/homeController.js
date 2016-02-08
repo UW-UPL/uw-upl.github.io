942f31097a0b17df34f10b2a2876465bf7ddaf0c
Zach Thomae <zthomae@gmail.com>
2016-02-08 14:53:09 -0600

    Add basic project table

--- scripts/controllers/homeController.js
+++ scripts/controllers/homeController.js
@@ -6,13 +6,28 @@ angular.module('upl-site').
         $scope.projects = [];
 
         Events.list().then(function(data) {
-            $scope.events = data;
+            $scope.events = data.Previous.slice(0, 3);
         }, function(data) {
             alert(data);
         });
 
         Projects.list().then(function(data) {
-            $scope.projects = data;
+            var left = true;
+            var row = 0;
+            for (var i = 0; i < data.length; i++) {
+                var d = data[i];
+                if (d.description.length > 100) {
+                    d.description = d.description.substring(0, 100) + "...";
+                }
+                if (left) {
+                    $scope.projects.push([d]);
+                    left = false;
+                } else {
+                    $scope.projects[row].push(d);
+                    left = true;
+                    row++;
+                }
+            }
         }, function(data) {
             alert(data);
         });
