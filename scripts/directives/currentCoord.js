212ada98c6b23f21eec125608d0fe02b419208ba
Zach Thomae <zthomae@gmail.com>
2016-02-24 15:39:59 -0600

    Remove dead code

--- scripts/directives/currentCoord.js
+++ scripts/directives/currentCoord.js
@@ -12,32 +12,6 @@ angular.module('upl-site')
 
       function updateTime() {
         currentCoordName.then(function(hourHolderMsg) {
-          /*
-          var madisonData = convertToMadisonTime(new Date());
-          var dayData = data[DAY_NAMES[madisonData.day]];
-
-          var hourHolder = null;
-
-          var hourKeys = Object.keys(dayData);
-          var nowTs = hoursAndMinutesToMillisecondsPastMidnight(madisonData);
-
-          //console.log('Looking for coord hours around: ', madisonData.hour + ':' + madisonData.minute, ' on a ', DAY_NAMES[madisonData.day]);
-
-          //NOTE: this could be a binary search if I wanted to be cool
-          var currentOfficeHour = hourKeys
-          .map(function(someHourKey) {
-            return {
-              key: someHourKey,
-              ts:  hourKeyToMillisecondsPastMidnight(someHourKey)
-            };
-          })
-          .find(function(obj) {
-            //console.log(obj.ts + '<=' + nowTs + '<=' + (obj.ts + COORD_HOUR_DURATION));
-            return isOfficeHour(obj.ts, nowTs);
-          });
-
-          hourHolder = currentOfficeHour ? dayData[currentOfficeHour.key] : null;
-          */
           // TODO: show/hide element if there is a coord
           if (hourHolderMsg) {
             console.log(hourHolderMsg);
