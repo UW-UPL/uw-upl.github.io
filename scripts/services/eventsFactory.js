5fa754afe04a90d8a9ebce7f0ea1bcd185876439
LOZORD <ljrudberg@gmail.com>
2016-06-23 23:31:19 -0500

    Weekly repeat events mostly work

--- scripts/services/eventsFactory.js
+++ scripts/services/eventsFactory.js
@@ -18,7 +18,8 @@ angular.module('upl-site')
 
 
 
-        var now = Date.now();
+        const nowDate = new Date();
+        const nowTimestamp = nowDate.getTime();
 
 
 
@@ -29,7 +30,97 @@ angular.module('upl-site')
         };
 
 
+        // TODO: accounts for "off-by-one-hour" events due to DST
+        // see http://stackoverflow.com/questions/11887934/check-if-daylight-saving-time-is-in-effect-and-if-it-is-for-how-many-hours
+        var createRepeatingEvents = function (repeatingEvent) {
+          const repeatData = repeatingEvent.repeats;
 
+          // handle garbage
+          if (!repeatData) {
+            return [];
+          }
+
+          const startDate = new Date(repeatingEvent.date);
+          const when = repeatData.when;
+          const ends = repeatData.ends;
+          const endDate = ends ? new Date(ends) : null;
+
+          const endsBeforeNow = endDate && endDate.getTime() <= nowTimestamp;
+
+          if (endsBeforeNow) {
+            // if the event has already finished repeating,
+            // there's no need to add a new repeat event
+            return [];
+          }
+
+          const startsAfterNow = startDate.getTime() >= nowTimestamp;
+
+          if (startsAfterNow) {
+            // if the event starts (repeating) after now,
+            // there's no need to create an other repeat events
+            return [];
+          }
+
+          var repeatOffset;
+
+          // what kind of repeat type is it?
+          switch(when) {
+            case 'weekly':
+              repeatOffset = 7 * 24 * 60 * 60 * 1000;
+              break;
+            default:
+              return [];
+          }
+
+          const tooShortToRepeat =
+            endDate && (startDate.getTime() + repeatOffset >= endDate.getTime());
+
+          if (tooShortToRepeat) {
+            // if a single repeat goes beyond the end date,
+            // there is no need to create a new event
+            // (this is kind of a stupidity check :^|)
+            return [];
+          }
+
+          // we may return up to 2 events
+          // the event that was in between the first repeat and now
+          // (i.e. the last repeat of this event)
+          // and the next (upcoming) repeat event
+          var newEvents = [];
+
+          // now for a little math to get the most recent event BEFORE now
+          const timeElapsedBetweenNowAndStart = nowTimestamp - startDate.getTime();
+          // integer division
+          const numElapsedRepeats = Math.floor(timeElapsedBetweenNowAndStart / repeatOffset);
+
+          const newPreviousEventTimestamp =
+            startDate.getTime() + (numElapsedRepeats * repeatOffset);
+
+          const repeatingEventTitle = (when.toUpperCase()) + ' REPEAT: ' + repeatingEvent.title;
+
+          // if we can have a new previous repeating event,
+          if (startDate.getTime() + repeatOffset < nowTimestamp) {
+            var newPreviousEvent = angular.copy(repeatingEvent);
+
+            newPreviousEvent.date  = newPreviousEventTimestamp;
+            newPreviousEvent.title = repeatingEventTitle;
+
+            newEvents.push(newPreviousEvent);
+          }
+
+          // now for the upcoming repeating event
+          const newUpcomingEventTimestamp =
+            startDate.getTime() + ((numElapsedRepeats + 1) * repeatOffset);
+
+          var newUpcomingEvent = angular.copy(repeatingEvent);
+
+          newUpcomingEvent.date  = newUpcomingEventTimestamp;
+          newUpcomingEvent.title = repeatingEventTitle;
+
+          newEvents.push(newUpcomingEvent);
+
+          return newEvents;
+        };
 
 
         service.populate = function() {
@@ -40,7 +131,19 @@ angular.module('upl-site')
 
                 var events = {'Upcoming': [], 'Previous': []};
 
-                response.data.forEach(function(event) {
+                // First add any repeating events for processing
+                var addRepeats = function(accumulation, currentEvent) {
+
+                  // on failure, createRepeatingEvents returns an empty array
+                  // so nothing (bad) will happen if there IS a failure
+                  return accumulation.concat(
+                    currentEvent, createRepeatingEvents(currentEvent)
+                  );
+                };
+
+                var eventsData = response.data.reduce(addRepeats, []);
+
+                eventsData.forEach(function(event) {
 
 
                     // allow html in descriptions
@@ -100,7 +203,7 @@ angular.module('upl-site')
                     event.link = linkify(event);
 
                     // place each event its in correct partition
-                    if (event.timestamp >= now) {
+                    if (event.timestamp >= nowTimestamp) {
 
                         events['Upcoming'].push(event);
 
