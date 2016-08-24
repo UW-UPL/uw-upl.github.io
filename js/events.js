aaaf888fec3d56e35c885dd9cd65b12223c39444
MikeCook9994 <mjcook3@wisc.edu>
2016-08-23 19:53:39 -0500

    Updated events page to support recurring events

--- js/events.js
+++ js/events.js
@@ -1,11 +1,16 @@
 var eventsSection = $('#events');
-var previousEvents = $('<section><h2>Previous events</h2><div><ul class="event-list"></ul></div>');
+
+var previousEvents = $('<section><h2>Previous events</h2><div><ul class="onetime-event-list"></ul></div>');
 var previousEventsList = previousEvents.find('ul').first();
-var upcomingEvents = $('<section><h2>Upcoming events</h2><div><ul class="event-list"></ul></div>');
+
+var upcomingEvents = $('<section><h2>Upcoming events</h2><div><ul class="onetime-event-list"></ul></div>');
 var upcomingEventsList = upcomingEvents.find('ul').first();
-var events = $('.event');
-eventsSection.children('div').remove();
+
+var events = $('.onetime-event-list-container .event');
+eventsSection.children('.div').remove();
+
 var now = moment();
+
 events.each(function(i, event) {
   var dateElem = $(event).find('.event-date').first();
   var date = moment(dateElem.text(), 'YYYY-MM-DD h:mm A');
@@ -16,9 +21,11 @@ events.each(function(i, event) {
     upcomingEventsList.append(event);
   }
 });
+
 if (upcomingEventsList.children().length > 0) {
   eventsSection.append(upcomingEvents);
 }
+
 if (previousEventsList.children().length > 0) {
   eventsSection.append(previousEvents);
 }
