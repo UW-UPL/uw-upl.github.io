0646213a1ebea71be63dd8a839fd9ec1df09911d
sbaumohl <sbaumohl@wisc.edu>
2023-09-06 16:40:20 -0500

    grace, sam, discord button

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,10 @@ import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.upl.cs.wisc.edu",
+  site: "https://www.upl.cs.wisc.edu/",
   integrations: [mdx(), sitemap(), tailwind(), react()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
-  }
+  },
+  compressHTML: false // dunno why the default of true breaks a lot of things
 });
