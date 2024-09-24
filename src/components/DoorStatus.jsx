f7733a59f4c8dc155074bbc61ac681f4c563ff9f
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2024-09-23 21:56:41 -0500

    add "check our hours" when doors closed

--- src/components/DoorStatus.jsx
+++ src/components/DoorStatus.jsx
@@ -11,7 +11,7 @@ const DoorStatus = () => {
       const data = await response.json();
       const { door1, door2 } = data.status;
 
-      // determine if the UPL is open
+      // Determine if the UPL is open
       if (door1 === "open" && door2 === "open") {
         setIsOpen(true);
       } else {
@@ -19,39 +19,44 @@ const DoorStatus = () => {
       }
     } catch (error) {
       console.error("Error fetching door status:", error);
-      setIsOpen(false); // default to closed
+      setIsOpen(false); // Default to closed
     }
   };
 
   useEffect(() => {
     fetchDoorStatus();
-    const interval = setInterval(fetchDoorStatus, 500); // fetch every 10 seconds
+    const interval = setInterval(fetchDoorStatus, 10000); // Fetch every 10 seconds
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <a
-      className={`md:text-md text-sm inline-flex items-center w-fit text-black font-sans font-semibold py-2 px-3 rounded-md focus:outline outline-offset outline-3 outline-sky-300`}
-    >
-      <div className="mr-2 flex-shrink-0">
-        <FontAwesomeIcon icon={isOpen ? faDoorOpen : faDoorClosed} />
+    <div className="md:text-md text-sm inline-flex flex-col items-center w-fit text-black font-sans font-semibold py-2 px-3 rounded-md focus:outline outline-offset outline-3 outline-sky-300 gap-1">
+      <div className="inline-flex items-center">
+        <div className="mr-2 flex-shrink-0">
+          <FontAwesomeIcon icon={isOpen ? faDoorOpen : faDoorClosed} />
+        </div>
+        <p className="flex-nowrap m-0">
+          The doors are{" "}
+          <span
+            className={
+              isOpen === null
+                ? "text-gray-500 font-bold"
+                : isOpen
+                ? "text-green-600 font-bold"
+                : "text-red-600 font-bold"
+            }
+          >
+            {isOpen === null ? "loading..." : isOpen ? "open" : "closed"}
+          </span>
+          !
+        </p>
       </div>
-      <p className="flex-nowrap">
-        The doors are{" "}
-        <span
-          className={
-            isOpen === null
-              ? "text-gray-500 font-bold"
-              : isOpen
-              ? "text-green-600 font-bold"
-              : "text-red-600 font-bold"
-          }
-        >
-          {isOpen === null ? "loading..." : isOpen ? "open" : "closed"}
-        </span>
-        !
-      </p>
-    </a>
+      {!isOpen && isOpen !== null && (
+        <a href="/hours" className="text-blue-800 underline m-0">
+          Check our hours here.
+        </a>
+      )}
+    </div>
   );
 };
 
