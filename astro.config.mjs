fec8f121a4046c8aab9085b9fbcd65ec0b960ae7
Nico Salm <nico@salm.dev>
2025-01-26 21:17:20 -0600

    add support for new resources page

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,37 +3,32 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-
 import playformCompress from "@playform/compress";
+import { generateResourceRedirects } from "./src/utils/redirects";
 
-// https://astro.build/config
-export default defineConfig({
-  site: "https://www.upl.cs.wisc.edu/",
-  redirects: {
-    "/hours.html": "/hours",
-    "/blog.html": "/blog",
-    "/coords": "/about",
-    // blog post back-links
-    "/2022/02/25/incorrect-reification.html": "/blog/incorrect-reification/",
-    "/2022/02/02/learning-to-code-with-projects.html":
-      "/blog/learning-to-code-with-projects/",
-    "/2022/02/09/a-cautionary-tale-of-amazon-web-service-classes.html":
-      "/blog/a-cautionary-tale-of-amazon-web-service-classes/",
-    "/2022/03/17/exploiting-github-actions.html":
-      "/blog/exploiting-github-actions/",
-    "/2022/04/03/why-i-use-firefox.html": "/blog/why-i-use-firefox/",
-    "/2022/04/16/Game-Design-and-Education.html":
-      "/blog/game-design-and-education/",
-    "/2022/04/7/Artificial-Consciousness-and-Phenomenology.html":
-      "/blog/artificial-consciousness-and-phenomenology/",
-    "/2022/05/12/Using-GPT3-To-Write-A-Blog-Post.html":
-      "/blog/using-gpt3-to-write-a-blog-post/",
-  },
-  integrations: [sitemap(), tailwind(), react(), playformCompress()],
-  compressHTML: false,
-  markdown: {
-    syntaxHighlight: "prism",
-    remarkPlugins: [remarkReadingTime],
-    optimize: true,
-  },
-});
+const createConfig = async () => {
+  const redirects = await generateResourceRedirects();
+
+  return defineConfig({
+    site: "https://www.upl.cs.wisc.edu/",
+    redirects,
+    integrations: [sitemap(), tailwind(), react(), playformCompress()],
+    compressHTML: false,
+    markdown: {
+      syntaxHighlight: "prism",
+      remarkPlugins: [remarkReadingTime],
+      optimize: true,
+      remarkRehype: {
+        footnoteLabel: 'Footnotes',
+        footnoteLabelProperties: {
+          className: ['sr-only']
+        }
+      }
+    },
+    build: {
+      format: 'file',
+    }
+  });
+};
+
+export default createConfig();
