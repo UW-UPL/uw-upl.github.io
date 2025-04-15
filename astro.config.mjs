024d54aef85e9528b33a52f4c4b2af758c6e85be
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2025-04-15 00:52:47 -0500

    more coord updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    prefetch: {
+        prefetchAll: true
+    }
+});
