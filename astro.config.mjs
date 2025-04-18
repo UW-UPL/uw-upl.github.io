e492601354556602263f6d187936751d7c8165bb
Michael Noguera <44954999+michaelnoguera@users.noreply.github.com>
2025-04-18 16:49:15 -0500

    Add door sensor (#199)
    * Add door sensor
    
    * CSS fixes for navbar alignment, standardize screen size breakpoints
    
    * Update package-lock.json
    
    * svelte component
    
    * add door icon logos
    
    ---------
    
    Co-authored-by: Andrew <74398819+AndrewDTR@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-    prefetch: {
-        prefetchAll: true
-    },
-    redirects: {
+  prefetch: {
+      prefetchAll: true
+  },
+
+  integrations: [svelte()],
+
+  redirects: {
         "/discord": {
           status: 302,
           destination: "https://discord.gg/dmefFzm"
---
+++ src/components/DoorStatus.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { onMount } from "svelte";
+  import Icon from "@iconify/svelte";
+
+  let status = "Loading door status...";
+  let openSave = null;
+
+  onMount(async () => {
+    try {
+      const res = await fetch("https://doors.amoses.dev/door-status");
+      const data = await res.json();
+
+      const back = data.find((d) => d.door === "back");
+      const front = data.find((d) => d.door === "front");
+      const open = back?.status === "on" && front?.status === "on";
+      openSave = open;
+
+      status = open
+        ? '...is <span style="color: green; font-weight: 600;">open</span> right now!'
+        : '...is <span style="color: rgb(183, 1, 1); font-weight: 600;">closed</span> right now.';
+    } catch {
+      status = "Error loading door status.";
+    }
+  });
+</script>
+
+<div
+  style="display: flex; align-items: center; justify-content: center; gap: 0.1rem;"
+>
+  <p style="margin: 0;">{@html status}</p>
+  {#if openSave}
+    <Icon icon="material-symbols:door-open-outline-rounded" />
+  {:else}
+    <Icon icon="material-symbols:door-front-outline-rounded" />
+  {/if}
+</div>
