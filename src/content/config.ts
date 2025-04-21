84cf4a5d4300171ba43b1b79b544c6ba1f10efbb
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2025-04-20 19:23:19 -0500

    built-in astro:image optimizations

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,35 +4,37 @@ import { defineCollection, z } from "astro:content";
 
 const coordinators = defineCollection({
   type: "data",
-  schema: z.object({
-    name: z.string(),
-    isActive: z.boolean(),
-    emoji: z.string(),
-    grade: z.string(),
-    image: z.string(),
-    study: z.optional(z.array(z.string())),
-    personalLink: z.optional(z.string()),
-    github: z.optional(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      isActive: z.boolean(),
+      emoji: z.string(),
+      grade: z.string(),
+      image: image(),
+      study: z.optional(z.array(z.string())),
+      personalLink: z.optional(z.string()),
+      github: z.optional(z.string()),
+    }),
 });
 
 const projects = defineCollection({
   type: "data",
-  schema: z.object({
-    title: z.string(),
-    source: z.string(),
-    writeup: z.optional(z.string()),
-    description: z.string(),
-    image: z.string(),
-    authors: z.array(z.string()),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      source: z.string(),
+      writeup: z.optional(z.string()),
+      description: z.string(),
+      image: image(),
+      authors: z.array(z.string()),
+    }),
 });
 
 const events = defineCollection({
   type: "data",
   schema: z.object({
     title: z.string(),
-    date: z.string(),   // ISO 8601 (https://dencode.com/en/date/iso8601)
+    date: z.string(), // ISO 8601 (https://dencode.com/en/date/iso8601)
     link: z.optional(z.string()),
     description: z.string(),
   }),
