7bc30ccac497fa2a068f5cf847033b6cdd0a2d7f
Andrew <74398819+AndrewDTR@users.noreply.github.com>
2025-09-05 20:21:39 -0500

    fix door stuff

--- src/components/DoorStatus.svelte
+++ src/components/DoorStatus.svelte
@@ -10,9 +10,7 @@
       const res = await fetch("https://doors.amoses.dev/door-status");
       const data = await res.json();
 
-      const back = data.find((d) => d.door === "back");
-      const front = data.find((d) => d.door === "front");
-      const open = back?.status === "on" && front?.status === "on";
+      const open = data.status === "open";
       openSave = open;
 
       status = open
